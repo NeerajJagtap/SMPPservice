43c5ba23210fa4b71202c70437819f5f70bac915
devendrakharpuriya <devendra.kharpuriya@gmail.com>
2016-10-17 12:30:27 +0530

    logger changes

--- src/com/vuclip/smpp/controllers/SmppController.java
+++ src/com/vuclip/smpp/controllers/SmppController.java
@@ -1,7 +1,5 @@
 package com.vuclip.smpp.controllers;
 
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
@@ -12,7 +10,12 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -23,9 +26,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.logica.smpp.pdu.Address;
 import com.logica.smpp.pdu.WrongLengthOfStringException;
+import com.vuclip.config.SmppConfig;
 import com.vuclip.smpp.client.CoreSMPPClient;
 import com.vuclip.smpp.to.PDUTO;
 import com.vuclip.smpp.to.SMPPReqTO;
+import com.vuclip.util.LoggingBean;
 import com.vuclip.util.SmppUtil;
 
 /**
@@ -39,13 +44,37 @@ public class SmppController {
 
 	private static final Logger logger = LoggerFactory.getLogger(SmppController.class);
 
-	// @Value("${ip}")
-	private String ip = "127.0.0.1";
+	@Autowired
+	private LoggingBean loggingBean;
 
-	// @Value("${port}")
-	private String port = "8084";
+	@Value("${ip}")
+	private String ip;
+
+	@Value("${port}")
+	private String port;
+
+	@Value("${title}")
+	private String title;
+
+	@Value("${sendsmsTxt}")
+	public String sendsmsTxt;
+
+	@Value("${billingTxt}")
+	public String billingTxt;
+
+	@Value("${zeroPricePoint}")
+	public String zeroPricePoint;
+
+	@Value("${providerId}")
+	public String providerId;
+
+	@Value("${customerId}")
+	public String customerId;
 
 	private CoreSMPPClient coreSMPPClient = null;
+
+	@Autowired
+	private SmppConfig smppConfig;
 
 	/*
 	 * @Autowired HashDAO hashDAO;
@@ -69,7 +98,6 @@ public class SmppController {
 
 	@RequestMapping(value = "/sendsms", method = RequestMethod.GET)
 	public ResponseEntity getResp(HttpServletRequest request, HttpServletResponse response) {
-		// HashData hashData = hashDAO.getByCustomerNo(customerNo);
 		// Initialize SMPP Client
 		if (null == coreSMPPClient) {
 			coreSMPPClient = new CoreSMPPClient();
@@ -94,12 +122,15 @@ public class SmppController {
 		String PRODUCT = map.get("PRODUCT");
 		String PRICEPOINT = map.get("PRICEPOINT");
 
+		Date requestTime = new Date();
+		Date responseTime = new Date();
+
 		System.out.println("******************* Welcome SMPP Billing  ********************");
 		System.out.println(" Talend Input  : " + meta_data);
 		System.out.println("Talend Input username : " + username);
 		System.out.println("Talend Input password : " + password);
-		System.out.println("Talend Input to : " + to);
-		System.out.println("Talend Input from : " + from);
+		System.out.println("Talend Input (msisdn) to : " + to);
+		System.out.println("Talend Input (shortCode) from : " + from);
 		System.out.println("Talend Input dlr_mask : " + dlr_mask);
 		System.out.println("Talend Input text : " + text);
 		System.out.println("Talend Input meta_data : " + meta_data);
@@ -108,26 +139,11 @@ public class SmppController {
 		System.out.println("Talend Input PARTNER_ROLE_ID : " + PARTNER_ROLE_ID);
 		System.out.println("Talend Input PRODUCT : " + PRODUCT);
 		System.out.println("Talend Input PRICEPOINT : " + PRICEPOINT);
+		
 
-		if (logger.isDebugEnabled()) {
-			logger.debug("******************* Welcome SMPP Billing  ********************");
-			logger.debug("Talend Input username : " + username);
-			logger.debug("Talend Input password : " + password);
-			logger.debug("Talend Input to : " + to);
-			logger.debug("Talend Input from : " + from);
-			logger.debug("Talend Input dlr_mask : " + dlr_mask);
-			logger.debug("Talend Input text : " + text);
-			logger.debug("Talend Input meta_data : " + meta_data);
-			logger.debug("Talend Input dlr_url : " + dlr_url);
-			logger.debug("Talend Input message_payload : " + message_payload);
-			logger.debug("Talend Input PARTNER_ROLE_ID : " + PARTNER_ROLE_ID);
-			logger.debug("Talend Input PRODUCT : " + PRODUCT);
-			logger.debug("Talend Input PRICEPOINT : " + PRICEPOINT);
-
-		}
-		//Start
+		// Start
 		String transactionId = getTransactionIDForURL(dlr_url);
-		//Set inside DB
+		// Set inside DB
 
 		transIdToUrlMap.put(transactionId, dlr_url);
 
@@ -147,7 +163,6 @@ public class SmppController {
 		smppReqTO.setDlrURL(dlr_url);
 		smppReqTO.setTransId(transactionId);
 		smppReqTO.setPduto(pduto);
-
 		try {
 			coreSMPPClient.runSMSJob();
 			coreSMPPClient.submitSMSRequest(smppReqTO);
@@ -158,45 +173,13 @@ public class SmppController {
 			}
 		}
 
-		// Sending SMS to SMPP - End
-
-		/*
-		String decoded_meta_data = SmppUtil.decodeToUtf8(meta_data);
-		String finalRespString = "transid=a189296112773922T28092016163107E40&msisdn=%p&msg=%a&smscid=%i&smsid=%I&dlrsmsc=%A&dlr=%d&time=%t&state=%d";
-
-		System.out.println("Encoded Output : " + decoded_meta_data);
 		if (logger.isDebugEnabled()) {
-			logger.debug("Encoded Output : " + finalRespString);
+			logger.debug(loggingBean.logData(request, "talendResponse", "rawRequest", "rawResponse", requestTime, responseTime, to, transactionId, PRICEPOINT));
 		}
-
-		String redirectionURLString = "http://" + ip + ":" + port + "/oovs-timwe/notification?transid="
-				+ finalRespString;
-		URL redirectionURL = null;
-		try {
-			redirectionURL = new URL(redirectionURLString);
-		} catch (MalformedURLException e) {
-			if (logger.isErrorEnabled()) {
-				logger.error("redirectionURL : " + redirectionURLString);
-			}
-		}
-
-		if (null != redirectionURL) {
-			System.out.println(redirectionURL);
-		}
-
-		System.out.println("redirectionURL : " + redirectionURLString);
-		if (logger.isDebugEnabled()) {
-			logger.debug("redirectionURL : " + redirectionURLString);
-		}
-*/
-		/*
-		 * ModelAndView mav = new ModelAndView("redirect:" + redirectionURL);
-		 * return mav;
-		 */
 
 		return new ResponseEntity(HttpStatus.OK);
-		// return finalRespString;
 	}
+	
 
 	private String getTransactionIDForURL(String dlr_url) {
 		String transactionId = null;
@@ -219,65 +202,42 @@ public class SmppController {
 	}
 
 	/*
-	 * // @RequestMapping("/searchCustomer") public ModelAndView
-	 * searchCustomer(@RequestParam(required = false, defaultValue = "") String
-	 * customerNo) { ModelAndView mav = new ModelAndView("showCustomer");
-	 * List<HashData> hashData = hashDAO.searchCustomer(customerNo.trim());
-	 * mav.addObject("SEARCH_HASHDATA_RESULTS_KEY", hashData); return mav; }
+	 * // @RequestMapping("/searchCustomer") public ModelAndView searchCustomer(@RequestParam(required = false, defaultValue = "") String customerNo) { ModelAndView mav = new
+	 * ModelAndView("showCustomer"); List<HashData> hashData = hashDAO.searchCustomer(customerNo.trim()); mav.addObject("SEARCH_HASHDATA_RESULTS_KEY", hashData); return mav; }
 	 */
 
 	/*
 	 * 
-	 * // @RequestMapping("/viewAllCustomer") public ModelAndView
-	 * getAllCustomer() { ModelAndView mav = new ModelAndView("showCustomer");
-	 * List<HashData> hashData = hashDAO.getAllHashData();
-	 * mav.addObject("SEARCH_HASHDATA_RESULTS_KEY", hashData); return mav; }
+	 * // @RequestMapping("/viewAllCustomer") public ModelAndView getAllCustomer() { ModelAndView mav = new ModelAndView("showCustomer"); List<HashData> hashData =
+	 * hashDAO.getAllHashData(); mav.addObject("SEARCH_HASHDATA_RESULTS_KEY", hashData); return mav; }
 	 * 
-	 * // @RequestMapping(value="/saveCustomer", method=RequestMethod.GET)
-	 * public ModelAndView newuserForm() { ModelAndView mav = new
-	 * ModelAndView("newCustomer"); HashData hashData = new HashData();
-	 * mav.getModelMap().put("newCustomer", hashData); return mav; }
+	 * // @RequestMapping(value="/saveCustomer", method=RequestMethod.GET) public ModelAndView newuserForm() { ModelAndView mav = new ModelAndView("newCustomer"); HashData hashData
+	 * = new HashData(); mav.getModelMap().put("newCustomer", hashData); return mav; }
 	 * 
-	 * // @RequestMapping(value="/saveCustomer", method=RequestMethod.POST)
-	 * public String create(@ModelAttribute("newCustomer") HashData hashData,
-	 * BindingResult result, SessionStatus status) {
-	 * validator.validate(hashData, result); if (result.hasErrors()) { return
-	 * "newCustomer"; } hashDAO.save(hashData); status.setComplete(); return
+	 * // @RequestMapping(value="/saveCustomer", method=RequestMethod.POST) public String create(@ModelAttribute("newCustomer") HashData hashData, BindingResult result,
+	 * SessionStatus status) { validator.validate(hashData, result); if (result.hasErrors()) { return "newCustomer"; } hashDAO.save(hashData); status.setComplete(); return
 	 * "redirect:viewAllCustomer.htm"; }
 	 * 
-	 * // @RequestMapping(value="/updateCustomer", method=RequestMethod.GET)
-	 * public ModelAndView edit(@RequestParam("id") Integer id) { ModelAndView
-	 * mav = new ModelAndView("editCustomer"); HashData hashData =
-	 * hashDAO.getById(id); mav.addObject("editCustomer", hashData); return mav;
-	 * }
+	 * // @RequestMapping(value="/updateCustomer", method=RequestMethod.GET) public ModelAndView edit(@RequestParam("id") Integer id) { ModelAndView mav = new
+	 * ModelAndView("editCustomer"); HashData hashData = hashDAO.getById(id); mav.addObject("editCustomer", hashData); return mav; }
 	 * 
 	 * 
 	 * 
-	 * @RequestMapping(value = "/getCustomer", method = RequestMethod.GET)
-	 * public ModelAndView getCustomer(String customerNo) { ModelAndView mav =
-	 * new ModelAndView("editCustomer"); // HashData hashData =
-	 * hashDAO.getByCustomerNo(customerNo);
+	 * @RequestMapping(value = "/getCustomer", method = RequestMethod.GET) public ModelAndView getCustomer(String customerNo) { ModelAndView mav = new ModelAndView("editCustomer");
+	 * // HashData hashData = hashDAO.getByCustomerNo(customerNo);
 	 * 
-	 * // AOC flow starts here String endocedAOCString =
-	 * "4u%2Bz3oOVstniZS7VucmMc9ky0r2iBglcQkxAHudhj5p9jSXjH09mXTnPMLGKDINWYMzLD5r5F95b3VZ5CpKg1RoXI8cEzqDSiXuoqajviQU%3D";
-	 * // String endocedAOCString = //
-	 * "MoO6yxJ2ru4mbjvWVm5IOXxNBbFsMhb2uTCUcTe0N5XYvcxXVi5SUaVzCdZCnG7GDQ81JfZBonN%2B0z2lML6HEIUY6pIVOJxBoPrSfrcfCzQ%3D";
-	 * String finalRespString = aocMockService.aocFlowMock(endocedAOCString);
-	 * System.out.println("Encoded Output from AOC : " + finalRespString);
-	 * logger.debug("Encoded Output from AOC : " + finalRespString);
+	 * // AOC flow starts here String endocedAOCString = "4u%2Bz3oOVstniZS7VucmMc9ky0r2iBglcQkxAHudhj5p9jSXjH09mXTnPMLGKDINWYMzLD5r5F95b3VZ5CpKg1RoXI8cEzqDSiXuoqajviQU%3D" ; //
+	 * String endocedAOCString = // "MoO6yxJ2ru4mbjvWVm5IOXxNBbFsMhb2uTCUcTe0N5XYvcxXVi5SUaVzCdZCnG7GDQ81JfZBonN%2B0z2lML6HEIUY6pIVOJxBoPrSfrcfCzQ%3D" ; String finalRespString =
+	 * aocMockService.aocFlowMock(endocedAOCString); System.out.println("Encoded Output from AOC : " + finalRespString); logger.debug("Encoded Output from AOC : " +
+	 * finalRespString);
 	 * 
 	 * mav.addObject("editCustomer", finalRespString); return mav; }
 	 * 
-	 * // @RequestMapping(value="/updateCustomer", method=RequestMethod.POST)
-	 * public String update(@ModelAttribute("editCustomer") HashData hashData,
-	 * BindingResult result, SessionStatus status) {
-	 * validator.validate(hashData, result); if (result.hasErrors()) { return
-	 * "editCustomer"; } hashDAO.update(hashData); status.setComplete(); return
+	 * // @RequestMapping(value="/updateCustomer", method=RequestMethod.POST) public String update(@ModelAttribute("editCustomer") HashData hashData, BindingResult result,
+	 * SessionStatus status) { validator.validate(hashData, result); if (result.hasErrors()) { return "editCustomer"; } hashDAO.update(hashData); status.setComplete(); return
 	 * "redirect:viewAllCustomer.htm"; }
 	 * 
-	 * // @RequestMapping("deleteCustomer") public ModelAndView
-	 * delete(@RequestParam("id") Integer id) { ModelAndView mav = new
-	 * ModelAndView("redirect:viewAllCustomer.htm"); hashDAO.delete(id); return
-	 * mav; }
+	 * // @RequestMapping("deleteCustomer") public ModelAndView delete(@RequestParam("id") Integer id) { ModelAndView mav = new ModelAndView("redirect:viewAllCustomer.htm");
+	 * hashDAO.delete(id); return mav; }
 	 */
 }
--- src/com/vuclip/util/HibernateSupportDAO.java
+++ src/com/vuclip/util/HibernateSupportDAO.java
@@ -10,7 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Repository
 public class HibernateSupportDAO {
 
-	@Autowired
+	@Autowired(required=true)
 	public SessionFactory sessionFactory;
 
 	public void setSessionFactory(SessionFactory sessionFactory){
